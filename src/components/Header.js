95cafd9e7ae74a91a708bc0b2533847675a314d2
Joel Nataren <65203252+joelnataren9@users.noreply.github.com>
2020-11-04 22:57:26 -0600

    Header terminado

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import logo from '../images/logo.png';
+import './styles/Header.css'
+
+function Header () {
+    return(
+        <nav id='header' className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt="Logo de Taki" className='logo' />
+                    Conf Hawaii
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">La conferencia</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Los oradores</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">El lugar y la fecha</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Conviértete en orador</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-taki" href="#">Comprar tickets</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
